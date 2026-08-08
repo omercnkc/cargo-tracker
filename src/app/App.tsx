2d128ab5eb0f6baf3188c63218e368ebc0de3b5a
ömer çanakçı <250541117@firat.edu.tr>
2026-08-08 16:24:35 +0300

    fix: App.tsx status bar koyu tema destegi eklendi

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,28 +3,40 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+
 import RootNavigator from '../navigation/RootNavigator';
 import { useAuthStore } from '../store/auth.store';
-
+import { useTheme } from '../theme/useTheme';
 import ErrorBoundary from '../components/common/ErrorBoundary';
 
 const queryClient = new QueryClient();
 
-const App = () => {
+const AppContent = () => {
   const initializeAuth = useAuthStore((state) => state.initializeAuth);
+  const { isDarkMode } = useTheme();
 
   useEffect(() => {
     initializeAuth();
   }, [initializeAuth]);
 
   return (
+    <>
+      <StatusBar style={isDarkMode ? 'light' : 'dark'} animated={true} />
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </>
+  );
+};
+
+const App = () => {
+  return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ErrorBoundary>
         <QueryClientProvider client={queryClient}>
           <SafeAreaProvider>
-            <NavigationContainer>
-              <RootNavigator />
-            </NavigationContainer>
+            <AppContent />
           </SafeAreaProvider>
         </QueryClientProvider>
       </ErrorBoundary>
